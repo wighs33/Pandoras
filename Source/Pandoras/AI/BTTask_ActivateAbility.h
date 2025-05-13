845b7af6cdfc4db9486f8bedbeec6d7ca01fe9e0
wighs33 <genji@kakao.com>
2025-05-14 08:42:32 +0900

    AI가 플레이어를 쫒아가서 공격

---
+++ Source/Pandoras/AI/BTTask_ActivateAbility.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlueprintBase.h"
+#include "BTTask_ActivateAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PANDORAS_API UBTTask_ActivateAbility : public UBTTask_BlueprintBase
+{
+	GENERATED_BODY()
+	
+	//virtual EBTNodeResult::Type ExecuteTask(
+	//	UBehaviorTreeComponent& OwnerComp,
+	//	uint8* NodeMemory
+	//) override
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "C++")
+	FGameplayTagContainer AbilityTags;
+
+	UPROPERTY(BlueprintReadWrite, Category = "C++", meta = (HideInDetailPanel))
+	int32 TargetActivateCount;
+
+	UPROPERTY(BlueprintReadWrite, Category = "C++", meta = (HideInDetailPanel))
+	int32 ActivatedCount;
+
+	UPROPERTY(BlueprintReadWrite, Category = "C++", meta = (HideInDetailPanel))
+	int32 MaxActivateCount;
+};
