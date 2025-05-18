6a036849797735616989b197f96c3fc8559cb20a
wighs33 <genji@kakao.com>
2025-05-18 16:27:46 +0900

    공격 타이밍에 맞춰서 가드 누르면 패링 발생하도록 구현

--- Source/Pandoras/Interface/ItemInterface.h
+++ Source/Pandoras/Interface/ItemInterface.h
@@ -19,18 +19,23 @@ class PANDORAS_API IItemInterface
 	GENERATED_BODY()
 	
 public:
+	// 검 궤적 이펙트 토글
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category="C++")
 	void ToggleSwordTrail(bool Enable);
 
+	// 출혈 이펙트 재생
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category="C++")
 	void TriggerBlood();
 
+	// 아이템 태그 반환
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category="C++", meta = (ReturnDisplayName = "WeaponTag"))
 	FGameplayTagContainer GetItemTag();
 
+	// 아이템 타입 반환
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "C++", meta = (ReturnDisplayName = "WeaponType"))
 	EItem GetItemType();
 
+	// 무기 떨어뜨리기
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "C++")
 	void Drop();
 };
