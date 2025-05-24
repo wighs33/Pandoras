b770a07efa230192d3eee546a6fe5e1866203559
wighs33 <genji@kakao.com>
2025-05-24 22:53:00 +0900

    AI가 다수의 적을 감지할 때 가까운 적에 우선순위를 둬서 포커싱
    - 행동트리 서비스 노드를 통해 2초마다 타겟 갱신

---
+++ Source/Pandoras/AI/AI_Interface.h
@@ -0,0 +1,24 @@
+// AI 인터페이스
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "AI_Interface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UAI_Interface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class PANDORAS_API IAI_Interface
+{
+	GENERATED_BODY()
+
+public:
+	// 타겟 갱신
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "C++")
+	void UpdateTargetActor();
+};
--- Source/Pandoras/AI/PandorasAIController.h
+++ Source/Pandoras/AI/PandorasAIController.h
@@ -16,11 +16,27 @@ class PANDORAS_API APandorasAIController : public ADetourCrowdAIController
 public:
 	// 생성자
 	APandorasAIController();
-	
+
+protected:
+	// 타겟 갱신
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "C++", meta = (ReturnDisplayName = "ClosestActor"))
+	AActor* GetClosestDetectedActor();
+
+// 컴포넌트
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "C++", meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<UAIPerceptionComponent> Sight;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "C++", meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<UAIPerceptionComponent> Hearing;
+
+// 변수
+protected:
+	// 현재 타겟
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "C++")
+	TObjectPtr<AActor> CurrentTarget;
+
+	// 감지된 모든 타겟
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "C++")
+	TArray<AActor*> DetectedTargets;
 };
